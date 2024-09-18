9bcbba3d9801542684a1f25425c7768e8b8ebaeb
Slava Meriton <slavameriton@gmail.com>
2024-09-18 22:08:46 +0300

    сохранила старую структуру классов проекта. Переименовала класс и методы из main.py  в более интуитивную форму. Добавила комеентариев с поямнениями и отметила места которые хочу переписать

--- main.py
+++ main.py
@@ -1,49 +1,109 @@
-from agents import QLearningAgent, Altruistic_agent
-from env import WorldEnv
-import matplotlib.pyplot as plt
-from typing import List, Optional
-import os
-import json
+from agents import QLearningAgent, Altruistic_agent  # берем классы агентов из соседнего файла
+from env import WorldEnv  # берем класс среды из соседнего файла
+#  import matplotlib.pyplot as plt # ЗАЧЕМ? ПРОКЛЯТО? Проверить
+from typing import Optional  # раньше тут вызывался еще и List
+import os  # для Q table нужно
+import json  # # для Q table нужно
 
-class Main_Frame():
 
-    def venv_init(self, patron_num, altruist_num, render_mode):
+class SimulationManager:
+    """
+    Что делает этот класс:
+        Управляет вызовом всех необходимых методов (run_simulation)
+        Инициализирует среду
+        Добавляет заданное количество агентов
+        Запускает процесс обучения агентов
+        Выполняет один эпизод взаимодействия агентов со средой
+        Тестирует обученных агентов без дальнейшего обучения
+        Сохраняет текущие Q-таблицы агентов на диск
+        Преобразует ключи Q-таблицы в строки для возможности сохранения в JSON
+    """
+
+    def run_simulation(
+            self,
+            progon_number: Optional[int] = None,
+            learning_flag: bool = True,
+            testing_flag: bool = True
+    ):
+        # ПЕРЕПИСАТЬ НА ЛОГИКУ по запуску скрипта для КОНЕЧНОГО ПОЛЬЗОВАТЕЛЯ ***
+        # Управляет вызовом всех необходимых методов -
+        # определяет какая ветка будет запущена:
+        #  - обучение патрона или альтруиста,
+        #  - демонстрация их поведения,
+        #  - демонстрация среды со случайными действиями итд)
+        if learning_flag:
+            self.learning()
+        else:
+            self.load_tables(progon_number)
+        if testing_flag:
+            self.show_trained_behavior()
+
+    def learning(
+            self,
+            patron_num: int = 1,
+            altruist_num: int = 1,
+            render_mode: str = "rgb_array",
+            num_episodes: int = 1000
+    ):
+        # Этот метод запускает процесс обучения агентов
+        # Я ВООБЩЕ НЕ ПОНЯЛА ЭТУ ЛОГИКУ. ОБСУДИТЬ ***
+
+        rewards = []
+
+        # Инициализация среды и агентов
+        self.init_environment_and_agents(patron_num, altruist_num, render_mode)
+
+        # Запуск цикла обучения
+        for episode in range(num_episodes):
+            total_reward, steps = self.run_simulation_step(learning_flag=True)
+            rewards.append(steps)
+            print(f"Episode {episode + 1}: Total Reward = {total_reward}, Steps - {steps}")
+        self.cache_tables()
+        # self.build_plot(rewards)
+
+        print("Episode finished!")
+
+        self.env.close()
+
+    def init_environment_and_agents(self, patron_num, altruist_num, render_mode):
         self.env = WorldEnv(render_mode=render_mode)
         self.add_agents(patron_num, altruist_num)
 
     def add_agents(self, patron_num, altruist_num):
+        # переписать без циклового вызова методов add_agents и env
         for counter in range(patron_num):
             self.env.agents[f"patron_{counter}"] = QLearningAgent(self.env.action_space())
         for counter in range(altruist_num):
             self.env.agents[f"altruist_{counter}"] = Altruistic_agent(self.env.action_space())
 
-    def learning(self, patron_num: int = 1, altruist_num: int = 1, render_mode: str = "rgb_array", num_episodes: int = 1000):
-        self.venv_init(patron_num, altruist_num, render_mode)
-        rewards = []
-        for episode in range(num_episodes):
-            total_reward, steps = self.progon(learning_flag=True)
-            rewards.append(steps)
-            print(f"Episode {episode + 1}: Total Reward = {total_reward}, Steps - {steps}")
-        self.cache_tables()
-        self.build_plot(rewards)
-        print("Episode finished!")
-        self.env.close()
-
-    def checking_learning(self, patron_num: int = 1, altruist_num: int = 1, render_mode: str = "human", num_episodes: int = 10):
+    def show_trained_behavior(
+            self,
+            patron_num: int = 1,
+            altruist_num: int = 1,
+            render_mode: str = "human",
+            num_episodes: int = 10
+    ):
         # Устанавливаем epsilon на минимальное значение и переводим в режим наблюдения
-        self.venv_init(patron_num, altruist_num, render_mode)
+        self.init_environment_and_agents(patron_num, altruist_num, render_mode)
         for agent_id, agent_instance in self.env.agents.items():
             agent_instance.epsilon = 0.01
         # Запускаем агента для тестирования его поведения
         for episode in range(num_episodes):
-            total_reward, steps = self.progon(learning_flag=False)
+            total_reward, steps = self.run_simulation_step(learning_flag=False)
             print(f"Test Episode {episode + 1}: Total Reward = {total_reward}, Steps - {steps}")
         self.env.close()
 
-    def progon(self, learning_flag: bool, steps: int = 0, total_reward: int = 0, action: dict = {}, possible_actions: int = 300, done = False):
+    def run_simulation_step(
+            self,
+            learning_flag: bool,
+            steps: int = 0,
+            total_reward: int = 0,
+            action: dict = {},
+            possible_actions: int = 300,
+            done=False):
         state, _ = self.env.reset()
         state_tupled = tuple(state.values())
-        while possible_actions>0 and not done:
+        while possible_actions > 0 and not done:
             steps += 1
             for agent_id, agent_instance in self.env.agents.items():
                 old_agent_location = agent_instance.location
@@ -84,37 +144,60 @@ class Main_Frame():
             new_table[str_key] = value
         return new_table
 
-    def load_tables(self, progon_number: int, cache_dir: str = "cache", try_dir_base: str = "progon_"):
+    def load_tables(self, progon_number: int = None, cache_dir: str = "cache", try_dir_base: str = "progon_"):
+        if progon_number is None:
+            existing_folders = [f for f in os.listdir(cache_dir) if
+                                f.startswith(try_dir_base) and os.path.isdir(os.path.join(cache_dir, f))]
+            if existing_folders:
+                max_i = max([int(f.split('_')[1]) for f in existing_folders])
+                progon_number = max_i
+            else:
+                raise ValueError("Нет сохранённых прогонов для загрузки.")
+
         progon_folder = os.path.join(cache_dir, f"{try_dir_base}{progon_number}")
         if not os.path.exists(progon_folder):
             raise ValueError(f"Попытка {progon_number} не существует.")
-        table_patron_path = os.path.join(progon_folder, "table_patron.json")
-        table_altruist_path = os.path.join(progon_folder, "table_altruist.json")
-        if not os.path.exists(table_patron_path) or not os.path.exists(table_altruist_path):
-            raise ValueError(f"Файлы table_1.json и/или table_2.json не найдены в папке {progon_folder}.")
-        # Загружаем table_1
-        with open(table_patron_path, 'r') as f1:
-            self.agent_patron.q_table = json.load(f1)
-        # Загружаем table_2
-        with open(table_altruist_path, 'r') as f2:
-            self.agent_altruist.q_table = json.load(f2)
-        print(f"Таблицы успешно загружены из {progon_folder}")
 
-    def build_plot(self, rewards: List):
-        plt.plot(rewards)
-        plt.xlabel('Episode')
-        plt.ylabel('Total Steps')
-        plt.title('Learning Progress')
-        plt.show()
+    # def load_tables(self, progon_number: int, cache_dir: str = "cache", try_dir_base: str = "progon_"):
+    #     progon_folder = os.path.join(cache_dir, f"{try_dir_base}{progon_number}")
+    #     if not os.path.exists(progon_folder):
+    #         raise ValueError(f"Попытка {progon_number} не существует.")
+    #     table_patron_path = os.path.join(progon_folder, "table_patron.json")
+    #     table_altruist_path = os.path.join(progon_folder, "table_altruist.json")
+    #     if not os.path.exists(table_patron_path) or not os.path.exists(table_altruist_path):
+    #         raise ValueError(f"Файлы table_1.json и/или table_2.json не найдены в папке {progon_folder}.")
+    #     # Загружаем table_1
+    #     with open(table_patron_path, 'r') as f1:
+    #         self.agent_patron.q_table = json.load(f1)
+    #     # Загружаем table_2
+    #     with open(table_altruist_path, 'r') as f2:
+    #         self.agent_altruist.q_table = json.load(f2)
+    #     print(f"Таблицы успешно загружены из {progon_folder}")
 
-    def main_frame(self, progon_number: Optional[int] = None, learning_needed: bool = True, testing_needed: bool = True):
-        if learning_needed:
-            self.learning()
-        else:
-            self.load_tables(progon_number)
-        if testing_needed:
-            self.checking_learning()
+    # def build_plot(self, rewards: List):
+    #     plt.plot(rewards)
+    #     plt.xlabel('Episode')
+    #     plt.ylabel('Total Steps')
+    #     plt.title('Learning Progress')
+    #     plt.show()
 
-
+#
+#
+#
+# Блок в котором определяется поведение скрипта
+# (по умолчанию при вызове напрямую агенты и обучаются и тестируют, агрументами вызова это можно поменять)
 if __name__ == "__main__":
-    Main_Frame().main_frame()
+    import sys  # Это для обработки аргументов командной строки
+
+    # по умолчанию запускается на обучение агента + демонстрацию (как и было)
+    learning_needed = True
+    testing_needed = True
+
+    # можно передать из командной строки агрументы no_learn или no_test и тогда будет что-то одно
+    # ПЕРЕПИСАТЬ НА ЛОГИКУ КОНЕЧНОГО ПОЛЬЗОВАТЕЛЯ ***
+    if "no_learn" in sys.argv:
+        learning_needed = False
+    if "no_test" in sys.argv:
+        testing_needed = False
+    SimulationManager().run_simulation(learning_flag=learning_needed, testing_flag=testing_needed)
+
